be9ec818dfcd642d8ec8a0a768c6232341dd9c1a
zakafz <zakaryfofana@proton.me>
2025-12-13 04:11:14 -0500

    Add Vercel Analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -74,6 +75,7 @@ export default function RootLayout({
         >
           <div className="root flex flex-col items-center">{children}</div>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
